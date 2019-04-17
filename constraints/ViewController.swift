77bedd845c16b2c1f4c2602613fb36231007b5e1
Leonardo Valbuena <leov.calderon@gmail.com>
2019-04-17 13:25:07 -0500

    lógicamente de la app

--- constraints/ViewController.swift
+++ constraints/ViewController.swift
@@ -13,10 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottonConstraint: NSLayoutConstraint!
     
     
+    
+    @IBOutlet weak var total: UILabel!
+    
+    @IBOutlet weak var descuento: UILabel!
+    
+    @IBOutlet weak var cantidadText: UITextField!
+    
+    @IBOutlet weak var porcentajeText: UITextField!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pantalla()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name:
+            UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name:
+            UIResponder.keyboardWillHideNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(teclado(notificacion:)), name:
+            UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func teclado(notificacion: Notification) {
+        
+        guard let tecladoUp = (notificacion.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {return}
+        
+        if notificacion.name == UIResponder.keyboardWillShowNotification {
+            if UIScreen.main.nativeBounds.height == 1136 {
+                self.view.frame.origin.y = -tecladoUp.height
+            }
+        } else {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     /**
@@ -43,7 +81,43 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    
+    @IBAction func calcular(_ sender: UIButton) {
+        self.view.endEditing(true)
+        
+        if cantidadText.text != "" && porcentajeText.text != "" {
+            guard let numero = cantidadText.text else { return }
+            guard let porc = porcentajeText.text else { return }
+        
+        
+            let cant = (numero as NSString).floatValue
+            print(cant)
+            let porciento = (porc as NSString).floatValue
+        
+            let desc = cant * porciento / 100
+            let totalFinal = cant - desc
+        
+            total.text = "$\(totalFinal)"
+            descuento.text = "$\(desc)"
+            
+        } else {
+            let alert = UIAlertController(title: "alerta", message: "escribe cantidad y porcentaje", preferredStyle: .alert)
+            let accion = UIAlertAction(title: "ok", style: .default, handler: nil)
+            alert.addAction(accion)
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 
-
+    @IBAction func limpiar(_ sender: UIButton) {
+        self.view.endEditing(true)
+        total.text = "$0.00"
+        descuento.text = "$0.00"
+        cantidadText.text = ""
+        porcentajeText.text = ""
+    }
+    
 }
 
